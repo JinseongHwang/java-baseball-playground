87fed1f44a6e1eeae07ae9f84a620753614adbcd
JinseongHwang <eddy5360@naver.com>
2021-07-16 17:24:56 +0900

    test: substring, charAt 메서드 구현

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,8 +1,11 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
     @Test
@@ -28,5 +31,37 @@ public class StringTest {
         assertThat(splited).contains("1");
         assertThat(splited).contains("2");
         assertThat(splited).containsExactly("1", "2");
+    }
+
+    @Test
+    void substring() throws Exception {
+        // given
+        String str = "(1,2)";
+
+        // when
+        String substr = str.substring(1, str.length() - 1);
+
+        // then
+        assertThat(substr).isEqualTo("1,2");
+    }
+
+    @DisplayName("문자열 접근 범위를 벗어난 경우")
+    @Test
+    void charAt() throws Exception {
+        // given
+        String str = "abc";
+        
+        // when
+        // then
+        // 예외 타입 매칭
+        assertThatThrownBy(() -> {
+            char c = str.charAt(4);
+        }).isInstanceOf(StringIndexOutOfBoundsException.class);
+
+        // 예외 발생 시 반환되는 메시지 매칭
+        assertThatExceptionOfType(IndexOutOfBoundsException.class)
+            .isThrownBy(() -> {
+                str.charAt(15);
+            }).withMessageMatching("String index out of range: \\d+");
     }
 }
