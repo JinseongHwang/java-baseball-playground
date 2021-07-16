326f1a7fcc765343c8d6738246f9555b45f7b069
JinseongHwang <eddy5360@naver.com>
2021-07-16 15:44:10 +0900

    test: split 메서드 구현

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -10,4 +10,23 @@ public class StringTest {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
     }
+
+    @Test
+    void split() throws Exception {
+        // given
+        String str = "1,2";
+
+        // when
+        String[] splited = str.split(",");
+
+        // then
+        assertThat(splited.length).isEqualTo(2);
+
+        assertThat(splited[0]).isEqualTo("1");
+        assertThat(splited[1]).isEqualTo("2");
+
+        assertThat(splited).contains("1");
+        assertThat(splited).contains("2");
+        assertThat(splited).containsExactly("1", "2");
+    }
 }
