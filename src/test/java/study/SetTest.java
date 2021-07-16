b0848e74e4968b4fe5a1636af289d38f9d8135f0
JinseongHwang <eddy5360@naver.com>
2021-07-16 18:36:46 +0900

    test: true, false에 유연한 테스트 코드 작성

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -8,6 +8,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class SetTest {
@@ -33,7 +34,7 @@ public class SetTest {
 
     @ParameterizedTest
     @ValueSource(ints = {1, 2, 3})
-    void contains(int arg) throws Exception {
+    void containsV1(int arg) throws Exception {
         // 중복 코드
 //        assertThat(numbers.contains(1)).isTrue();
 //        assertThat(numbers.contains(2)).isTrue();
@@ -43,4 +44,14 @@ public class SetTest {
         assertThat(numbers.contains(arg)).isTrue();
     }
 
+    @ParameterizedTest
+    @CsvSource(value = {"1,1", "2,1", "3,1", "4,0", "5,0"}, delimiter = ',')
+    void containsV2(String arg, String expected) throws Exception {
+        // given
+        boolean expectedBool = expected.equals("1");
+
+        // when
+        // then
+        assertThat(numbers.contains(Integer.parseInt(arg))).isEqualTo(expectedBool);
+    }
 }
